d05c3727b82a05e671f362915d1db2ed7ba8707f
yank <yankscally@gmail.com>
2025-09-15 14:59:42 +0100

    a few small things
    - added snake case helper script to convert PascalCase dirs and files to camel case
    - fixed the gallery opening incorrectly on plugin activation or editor restart
    - committing .import files

---
+++ convert_to_snake_case.gd
@@ -0,0 +1,44 @@
+@tool
+extends EditorScript
+
+var path = "res://godot_brush_materials/"
+
+func _run() -> void:
+	recurse(path)
+	print("Renaming complete!")
+
+
+func recurse(current_path: String) -> void:
+	var dir = DirAccess.open(current_path)
+	if dir == null:
+		print("Failed to open directory: ", current_path)
+		return
+
+	dir.list_dir_begin()
+	var file_name = dir.get_next()
+	
+	while file_name != "":
+		var full_path = current_path + "/" + file_name  # Concatenate paths manually
+		
+		if dir.current_is_dir():
+			# Recursively handle directories first
+			recurse(full_path)
+
+			# Rename the directory
+			var new_dir_name = file_name.to_snake_case()
+			if new_dir_name != file_name:
+				var new_full_path = current_path + "/" + new_dir_name
+				if dir.rename(full_path, new_full_path) != OK:
+					print("Failed to rename directory: ", full_path)
+		else:
+			# Rename files
+			var new_file_name = file_name.to_snake_case()
+			if new_file_name != file_name:
+				var new_full_path = current_path + "/" + new_file_name
+				if dir.rename(full_path, new_full_path) != OK:
+					print("Failed to rename file: ", full_path)
+		
+		file_name = dir.get_next()
+	
+	dir.list_dir_end()
+	
