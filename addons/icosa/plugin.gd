00967eb6a8c5f4e96391a0dda555c6e5ff09930d
Andy Baker <andy@andybak.net>
2025-10-03 12:12:56 +0100

    Wire up lighting uniforms

--- addons/icosa/plugin.gd
+++ addons/icosa/plugin.gd
@@ -3,9 +3,11 @@ extends EditorPlugin
 func _get_plugin_name(): return "Icosa Gallery"
 func _get_plugin_icon(): return EditorInterface.get_editor_theme().get_icon("GridMap", "EditorIcons") #return load("res://addons/icosa-gallery/logo/logo_tiny.png")
 const MainPanel = preload("res://addons/icosa/browser.tscn")
+const LightSync = preload("res://addons/icosa/icosa_light_sync.gd")
 var main_panel_instance
 
 var gltf : IcosaGLTF
+var light_sync_instance
 
 
 func _enter_tree():
@@ -14,21 +16,65 @@ func _enter_tree():
 	gltf = IcosaGLTF.new()
 	GLTFDocument.register_gltf_document_extension(gltf)
 	main_panel_instance.visible = false
-	
+
 	var settings = EditorInterface.get_editor_settings()
 	settings.set_setting("docks/filesystem/other_file_extensions", "ico,icns,bin")
 
-	
+	# Register global shader parameters for Icosa brush lighting
+	_register_global_shader_parameters()
+
+	# Add light sync node to keep shader parameters updated
+	light_sync_instance = LightSync.new()
+	add_child(light_sync_instance)
+
+
 func _exit_tree():
 	if main_panel_instance:
 		main_panel_instance.queue_free()
 	GLTFDocument.unregister_gltf_document_extension(gltf)
-	
+
+	# Clean up light sync
+	if light_sync_instance:
+		light_sync_instance.queue_free()
+
+	# Remove global shader parameters
+	_unregister_global_shader_parameters()
+
 	var settings = EditorInterface.get_editor_settings()
 	settings.set_setting("docks/filesystem/other_file_extensions", "ico,icns")
+
 func _has_main_screen():
 	return true
 
-func _make_visible(visible): 
+func _make_visible(visible):
 	if main_panel_instance:
 		main_panel_instance.visible = visible
+
+func _register_global_shader_parameters():
+	# Light 0 (main light)
+	if not RenderingServer.global_shader_parameter_get("u_SceneLight_0_direction"):
+		RenderingServer.global_shader_parameter_add("u_SceneLight_0_direction",
+			RenderingServer.GLOBAL_VAR_TYPE_VEC3, Vector3(0, -1, 0))
+	if not RenderingServer.global_shader_parameter_get("u_SceneLight_0_color"):
+		RenderingServer.global_shader_parameter_add("u_SceneLight_0_color",
+			RenderingServer.GLOBAL_VAR_TYPE_COLOR, Color.WHITE)
+
+	# Light 1 (secondary light)
+	if not RenderingServer.global_shader_parameter_get("u_SceneLight_1_direction"):
+		RenderingServer.global_shader_parameter_add("u_SceneLight_1_direction",
+			RenderingServer.GLOBAL_VAR_TYPE_VEC3, Vector3(0, 1, 0))
+	if not RenderingServer.global_shader_parameter_get("u_SceneLight_1_color"):
+		RenderingServer.global_shader_parameter_add("u_SceneLight_1_color",
+			RenderingServer.GLOBAL_VAR_TYPE_COLOR, Color(0.5, 0.5, 0.5, 1.0))
+
+	# Ambient light
+	if not RenderingServer.global_shader_parameter_get("u_ambient_light_color"):
+		RenderingServer.global_shader_parameter_add("u_ambient_light_color",
+			RenderingServer.GLOBAL_VAR_TYPE_COLOR, Color(0.2, 0.2, 0.2, 1.0))
+
+func _unregister_global_shader_parameters():
+	RenderingServer.global_shader_parameter_remove("u_SceneLight_0_direction")
+	RenderingServer.global_shader_parameter_remove("u_SceneLight_0_color")
+	RenderingServer.global_shader_parameter_remove("u_SceneLight_1_direction")
+	RenderingServer.global_shader_parameter_remove("u_SceneLight_1_color")
+	RenderingServer.global_shader_parameter_remove("u_ambient_light_color")
